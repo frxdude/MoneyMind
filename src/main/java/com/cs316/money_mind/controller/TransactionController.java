7b7da9d0c0539f3f6119b2bafd2673749251a024
frxdude <goodaberg103@gmail.com>
2021-12-06 03:20:06 +0800

    security config changed, role added to requests

--- src/main/java/com/cs316/money_mind/controller/TransactionController.java
+++ src/main/java/com/cs316/money_mind/controller/TransactionController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -32,27 +33,32 @@ public class TransactionController {
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
+    @PreAuthorize("hasAnyRole('ROLE_CHILDREN','ROLE_YOUTH','ROLE_ADULT','ROLE_SENIOR')")
     public ResponseEntity<Object> list(@RequestParam int page, @RequestParam int size, HttpServletRequest req) {
         return ResponseEntity.ok(service.list(page, size, req));
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    @PreAuthorize("hasAnyRole('ROLE_CHILDREN','ROLE_YOUTH','ROLE_ADULT','ROLE_SENIOR')")
     public ResponseEntity<Object> get(@PathVariable(value = "id") Long id, HttpServletRequest req) throws BusinessException {
         return ResponseEntity.ok(service.get(id, req));
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
+    @PreAuthorize("hasAnyRole('ROLE_CHILDREN','ROLE_YOUTH','ROLE_ADULT','ROLE_SENIOR')")
     public ResponseEntity<Object> add(@Valid @RequestBody TransactionRequest addRequest, HttpServletRequest req) {
         service.add(addRequest, req);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    @PreAuthorize("hasAnyRole('ROLE_CHILDREN','ROLE_YOUTH','ROLE_ADULT','ROLE_SENIOR')")
     public ResponseEntity<Object> update(@PathVariable(value = "id") Long id, @Valid @RequestBody TransactionRequest updateRequest , HttpServletRequest req) throws BusinessException {
         return ResponseEntity.ok(service.update(id, updateRequest, req));
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    @PreAuthorize("hasAnyRole('ROLE_CHILDREN','ROLE_YOUTH','ROLE_ADULT','ROLE_SENIOR')")
     public ResponseEntity<Object> delete(@PathVariable(value = "id") Long id, HttpServletRequest req) throws BusinessException {
         service.delete(id, req);
         return ResponseEntity.noContent().build();
